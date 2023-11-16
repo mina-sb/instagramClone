6937485adb2557c137b4b604307e7495ec84332c
mina <sadeghbayan.mina@gmail.com>
2023-11-16 14:16:47 +0330

    Post component implemenetd

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Post.css';
 
 
-const Post = ({username}) => {
+const Post = ({ username, id, likelist }) => {
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    user && !user.likedPosts.includes(id) && setLiked(true);
+    user && !user.savedPosts.includes(id) && setSaved(true);
+  }, [liked]);
+
+  const likePost = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      if (liked) {
+        user.likedPosts.push(id);
+        localStorage.setItem("user", JSON.stringify(user));
+        setLiked((prevState) => !prevState);
+        likelist.push(user.userInfo.username);
+        console.log(likelist)
+      } else {
+        user.likedPosts.pop(id);
+        localStorage.setItem("user", JSON.stringify(user));
+        setLiked((prevState) => !prevState);
+        likelist.pop(user.userInfo.username);
+        console.log(likelist);
+
+      }
+    }
+  };
+
+  const savePost = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      if (saved) {
+        user.savedPosts.push(id);
+        localStorage.setItem("user", JSON.stringify(user));
+        setSaved((prevState) => !prevState);
+      } else {
+        user.savedPosts.pop(id);
+        localStorage.setItem("user", JSON.stringify(user));
+        setSaved((prevState) => !prevState);
+      }
+    }
+  };
+
   return (
     <div className="post-container">
       <div className="user-info">
@@ -16,14 +60,25 @@ const Post = ({username}) => {
       />
       <div className="post-buttons">
         <div>
-          <i class="bx bx-heart post-button mr-2 ml-1"></i>
+          {liked ? (
+            <i class="bx bx-heart post-button mr-2 ml-1" onClick={likePost}></i>
+          ) : (
+            <i
+              class="bx bxs-heart post-button mr-2 ml-1"
+              onClick={likePost}
+            ></i>
+          )}
           <i class="bx bx-message-rounded-dots bx-flip-horizontal post-button mr-2"></i>
           <i class="bx bx-send post-button mr-2"></i>
         </div>
-        <i class="bx bx-bookmark post-button"></i>
+        {saved ? (
+          <i class="bx bx-bookmark post-button" onClick={savePost}></i>
+        ) : (
+          <i class="bx bxs-bookmark-star post-button" onClick={savePost}></i>
+        )}
       </div>
       <div className="caption">
-        <b>{username}</b>{" "}
+        <b>{username}</b>
         <span>
           Also, this is inside a web browser (Safari) and only seems to be
           working on this account. My personal account still looks like a mobile
@@ -32,6 +87,6 @@ const Post = ({username}) => {
       </div>
     </div>
   );
-}
+};
 
 export default Post
