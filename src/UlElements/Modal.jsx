aaf15dc405db0dee7ff407336b8141e0cb08b058
mina <sadeghbayan.mina@gmail.com>
2023-11-17 11:22:41 +0330

    Modal component implemented

---
+++ src/UlElements/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./modal.css";
+
+const Modal = (props) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <h2 className="modal-title">Share Modal</h2>
+          <span className="close-button" onClick={props.onClose}>
+            &times;
+          </span>
+        </div>
+
+        <hr />
+        <p>Share on social media:</p>
+        <div className="social-icons">
+          <i class="bx bxl-instagram-alt"></i>
+          <i class="bx bxl-whatsapp"></i>
+          <i class="bx bxl-telegram"></i>
+          {/* Add more social media icons as needed */}
+        </div>
+        <button
+          onClick={props.onClose}
+          className="button main-color-button footer-close-btn"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal
