daaf279f82f33d781b442139b635f59c9208a9aa
mina <sadeghbayan.mina@gmail.com>
2023-11-17 11:24:09 +0330

    NewPost component implemented

---
+++ src/post/NewPost.jsx
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import { addPost } from "../store/postSlice";
+import { useNavigate } from "react-router";
+import "./NewPost.css";
+
+
+const NewPost = () => {
+  
+   const [caption, setCaption] = useState("");
+   const [media, setMedia] = useState(null);
+  const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+   const handleCaptionChange = (event) => {
+     setCaption(event.target.value);
+   };
+
+   const handleMediaChange = (event) => {
+     const file = event.target.files[0];
+     setMedia(file);
+   };
+
+   const handleCreatePost = () => {
+    
+
+     setCaption("");
+     setMedia(null);
+
+
+      const newPost = {
+        id: 1,
+        username: "ahmad",
+        img: "https://jovial-sammet-j3svpro5t.storage.iran.liara.space/products/1.jpg",
+        likes: [],
+        share: 0,
+        saves: [],
+        comments: [],
+      };
+     
+     dispatch(addPost(newPost));
+     navigate("/");
+   };
+
+
+
+  return (
+    <div className="post-container">
+      <h1 className="newPost-title">New Post</h1>
+      <div
+        style={{
+          borderRadius: "10px",
+        }}
+      >
+        {media && (
+          <img
+            src={URL.createObjectURL(media)}
+            alt="Uploaded Media"
+            style={{
+              width: "100%",
+            }}
+          />
+        )}
+        <textarea
+          placeholder="Write your caption..."
+          value={caption}
+          onChange={handleCaptionChange}
+          style={{
+            width: "calc(100% - 3.5rem)",
+            marginBottom: "10px",
+            padding: "10px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+            margin: "1rem 1rem 1rem",
+            outline: "none",
+          }}
+          rows={4}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleMediaChange}
+          style={{ marginBottom: "10px", marginLeft: "1rem" }}
+        />
+        <br />
+        <button
+          onClick={handleCreatePost}
+          style={{
+            marginLeft: "1rem",
+            backgroundColor: "#3897f0",
+            color: "white",
+            padding: "10px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            border: "none",
+            marginBottom: "1rem",
+          }}
+        >
+          Post
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NewPost
+
+
+
+
+
